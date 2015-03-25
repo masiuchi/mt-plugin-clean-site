35395607f8848edbdd0202e477dbb2b6e61f6648
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2015-03-25 20:54:34 +0900

    Remove old files and directories after changing website/blog path. #7

--- plugins/CleanSite/lib/CleanSite/Callback.pm
+++ plugins/CleanSite/lib/CleanSite/Callback.pm
@@ -108,6 +108,29 @@ sub blog_post_remove {
 
     require File::Path;
     File::Path::rmtree( $obj->site_path );
+
+    return 1;
+}
+
+sub remove_old_blog {
+    my ( $cb, $obj, $original ) = @_;
+
+    if ( !MT->config('DeleteFilesAtRebuild') || !MT->component('MoveAssets') )
+    {
+        return 1;
+    }
+
+    require File::Path;
+
+    if ( $obj->site_path ne $original->site_path ) {
+        File::Path::rmtree( $original->site_path );
+    }
+
+    if ( $obj->archive_path ne $original->archive_path ) {
+        File::Path::rmtree( $original->archive_path );
+    }
+
+    return 1;
 }
 
 sub category_post_remove {
